31dd314a466cd1e482e78182f78eefc9a0ac49e3
Saagar <Saagar.Ahluwalia@bellevuecollege.edu>
2022-06-21 12:54:00 -0700

    Add initial test cases

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class StringCalculatorTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StringCalculatorTest {
+    @Test
+    public void negativesThrow() {
+        assertThrows(Exception.class, () -> StringCalculator.add("-1,5"));
+    }
+
+    @Test
+    public void numbersGreaterThan1000AreIgnored() {
+        assertEquals(1, StringCalculator.add("1,1000"));
+    }
 }
